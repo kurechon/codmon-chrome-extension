9527df7795992988971c6aaa08e09cfd652e3fd0
kurechon <keisuke@tsukayoshi.com>
2023-04-15 22:36:22 +0900

    Codmon 以外のページでアイコンを日活性に切り替え

--- src/background.js
+++ src/background.js
@@ -11,3 +11,21 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     });
   }
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete") {
+    const validDomain = "parents.codmon.com";
+
+    if (tab.url.includes(validDomain)) {
+      chrome.action.setIcon({
+        path: "icons/icon128.png",
+        tabId: tab.id,
+      });
+    } else {
+      chrome.action.setIcon({
+        path: "icons/icon_bw128.png",
+        tabId: tab.id,
+      });
+    }
+  }
+});
